c2a7f63839d9ffa54f99c1f95f5230547553bbb5
unknown <kumarabhishek0947@gmail.comgit config --global user.name AAbhishek18>
2022-05-30 11:30:19 +0530

    Feture-4, createOrder and Update API

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const userController = require("../controllers/userController")
 const productController = require('../controllers/productController')
 const cartController = require('../controllers/cartController')
+const orderController = require('../controllers/orderController')
 const { userAuth } = require('../middleware/auth')
+
 
 
 
@@ -13,6 +15,7 @@ router.post('/register', userController.createUser)
 router.post('/login', userController.loginUser)
 router.get('/user/:userId/profile', userAuth, userController.getUser)
 router.put('/user/:userId/profile', userAuth, userController.updateUserDetails)
+
 
 //@ PRODUCT ROUTE HANDLER
 router.post('/products', productController.createProduct)
@@ -25,6 +28,10 @@ router.delete('/products/:productId', productController.deleteproduct)
 router.post('/users/:userId/cart',cartController.createCart)
 router.put('/users/:userId/cart',cartController.updateCart)
 router.get('/users/:userId/cart',cartController.getCart)
+
+//ORDER ROUTE HANDLER
+router.post('/users/:userId/orders',orderController.createOrder)
+router.put('/users/:userId/orders',orderController.updateOrder)
 
 
 
